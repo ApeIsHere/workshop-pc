431fc9f6a486e7c219e337ef43976fb9fb699376
ApeIsHere <vladimir.volkov.mg@gmail.com>
2023-09-07 18:47:41 +0300

    Teachers Animation v0.1.1
    Fixed the bug with the event listener not being removed

--- dist/script.js
+++ dist/script.js
@@ -210,10 +210,22 @@ const tutors = function tutors() {
         teacherCards = document.querySelectorAll('.tutors__card'),
         buttons = document.querySelectorAll('.button-bio'),
         bios = document.querySelectorAll('.tutors__bio');
-  let position = {};
+  let position = {},
+      removeCallBacks = [];
+
+  const clearCallBacks = () => removeCallBacks.forEach(c => c());
+
+  const changeCssVar = (center = 0, initial = 0) => {
+    document.documentElement.style.setProperty('--initial-position', initial);
+    document.documentElement.style.setProperty('--center-position', center);
+  };
+
   buttons.forEach((btn, i) => {
-    function showBio() {
-      //Getting all cards x-position
+    console.log(removeCallBacks);
+
+    const showBio = () => {
+      console.log(btn); //Getting all cards x-position
+
       teacherCards.forEach((card, j) => {
         position[`x${j}`] = Math.floor(card.getBoundingClientRect().x);
       }); // Animating the correct card to the center using CSS variables to control @keyframe animation
@@ -228,26 +240,21 @@ const tutors = function tutors() {
         } else {
           if (clickedCardPos !== centerCardPos) {
             const distance = centerCardPos - clickedCardPos;
-            changeCssVar(`${distance}px`, `${-distance}px`);
-            card.style.animation = 'flowToCenter 1.2s ease-in-out forwards';
-            teacherItem[1].style.animation = 'flowToSide 1.2s ease-in-out forwards'; // we need to apply 2 diffirent animations to the same object
+            changeCssVar(`${distance}px`, `${-distance}px`); // we need to apply 2 diffirent animations to the same object
             // that's why we use a wrapper teacherItem
             // we apply 1 animation to the card and the second to the wrapper.
+
+            card.style.animation = 'flowToCenter 1.2s ease-in-out forwards';
+            teacherItem[1].style.animation = 'flowToSide 1.2s ease-in-out forwards';
           }
         }
       });
-      buttons.forEach(button => {
-        button.removeEventListener('click', showBio);
-      });
-    }
+      clearCallBacks();
+    };
 
     btn.addEventListener('click', showBio);
+    removeCallBacks.push(() => btn.removeEventListener('click', showBio));
   });
-
-  function changeCssVar(center = 0, initial = 0) {
-    document.documentElement.style.setProperty('--initial-position', initial);
-    document.documentElement.style.setProperty('--center-position', center);
-  }
 };
 
 tutors();
--- src/js/modules/tutors.js
+++ src/js/modules/tutors.js
@@ -1,57 +1,59 @@
 const tutors = function () {
 
-        const teacherItem = document.querySelectorAll('.tutors__item'),
-            teacherCards = document.querySelectorAll('.tutors__card'),
-            buttons = document.querySelectorAll('.button-bio'),
-            bios = document.querySelectorAll('.tutors__bio');
-        let position = {};
+    const teacherItem = document.querySelectorAll('.tutors__item'),
+        teacherCards = document.querySelectorAll('.tutors__card'),
+        buttons = document.querySelectorAll('.button-bio'),
+        bios = document.querySelectorAll('.tutors__bio');
+    let position = {},
+        removeCallBacks = [];
 
 
-        buttons.forEach((btn, i) => {
-    
-            function showBio() {
-                //Getting all cards x-position
-                teacherCards.forEach((card, j) => {
-                    position[`x${j}`] = Math.floor(card.getBoundingClientRect().x);
-                });
-                // Animating the correct card to the center using CSS variables to control @keyframe animation
-             teacherCards.forEach((card, j) => { 
-                    const clickedCardPos = position[`x${j}`],
-                        centerCardPos =  position.x1;
+    const clearCallBacks = () => removeCallBacks.forEach(c => c());
 
-                    if (j !== i) {
-                        card.style.opacity = 0.4;
-                        card.style.animation = 'getSmaller 1.2s ease-in-out forwards';
-                    } else {  
-                        if (clickedCardPos !== centerCardPos) {
+    const changeCssVar = (center = 0, initial = 0) => {
+        document.documentElement.style.setProperty('--initial-position', initial);
+        document.documentElement.style.setProperty('--center-position', center);
+    }
 
-                            const distance = centerCardPos - clickedCardPos;
-                            changeCssVar(`${distance}px`, `${-distance}px`);
+    buttons.forEach((btn, i) => {
+        console.log(removeCallBacks);
 
-                            card.style.animation = 'flowToCenter 1.2s ease-in-out forwards';
-                            teacherItem[1].style.animation = 'flowToSide 1.2s ease-in-out forwards'; 
-                            // we need to apply 2 diffirent animations to the same object
-                            // that's why we use a wrapper teacherItem
-                            // we apply 1 animation to the card and the second to the wrapper.
-                        }
+        const showBio = () => {
+            console.log(btn);
+            //Getting all cards x-position
+            teacherCards.forEach((card, j) => {
+                position[`x${j}`] = Math.floor(card.getBoundingClientRect().x);
+            });
+
+            // Animating the correct card to the center using CSS variables to control @keyframe animation
+            teacherCards.forEach((card, j) => {
+                const clickedCardPos = position[`x${j}`],
+                    centerCardPos = position.x1;
+
+                if (j !== i) {
+                    card.style.opacity = 0.4;
+                    card.style.animation = 'getSmaller 1.2s ease-in-out forwards';
+                } else {
+                    if (clickedCardPos !== centerCardPos) {
+                        const distance = centerCardPos - clickedCardPos;
+
+                        changeCssVar(`${distance}px`, `${-distance}px`);
+
+                        // we need to apply 2 diffirent animations to the same object
+                        // that's why we use a wrapper teacherItem
+                        // we apply 1 animation to the card and the second to the wrapper.
+                        card.style.animation = 'flowToCenter 1.2s ease-in-out forwards';
+                        teacherItem[1].style.animation = 'flowToSide 1.2s ease-in-out forwards';
                     }
-                });
-                
-                buttons.forEach(button => {
-                    button.removeEventListener('click', showBio);
-                });
-            }
+                }
+            });
 
-            btn.addEventListener('click', showBio);
-        });
+            clearCallBacks();
+        }
 
-
-
-        function changeCssVar(center = 0, initial = 0) {
-            document.documentElement.style.setProperty('--initial-position', initial);
-            document.documentElement.style.setProperty('--center-position', center);
-          }
-
+        btn.addEventListener('click', showBio);
+        removeCallBacks.push(() => btn.removeEventListener('click', showBio));
+    });
 }
 
 tutors();
